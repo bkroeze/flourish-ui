e5b81c91c35a83057c68ea950caebef4deaa2840
Bruce Kroeze <bkroeze@gmail.com>
2021-05-12 16:31:24 -0700

    Initial

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Flourish.Finance
+        </q-toolbar-title>
+
+        <div>(connect wallet)</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Github',
+    caption: 'github.com/bkroeze/flourish-ui',
+    icon: 'code',
+    link: 'https://github.com/bkroeze/flourish-ui'
+  },
+  {
+    title: 'Bruce\'s Twitter',
+    caption: '@CryptoArchon',
+    icon: 'rss_feed',
+    link: 'https://twitter.com/cryptoarchon'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page>
+    <div style="padding: 1em 0 0 1em">
+      <div class="row">
+      <div class="col-auto">
+        Flourish Finance, launched
+        <q-badge>12 May 2021, 1:50 PM PDT</q-badge>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <q-img src="~assets/flourish_finance_chart.png" height="477px" width="680px" />
+      </div>
+    </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
