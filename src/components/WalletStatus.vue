156610bfb63a97365ba17ac7809dbad328aed1b7
Bruce Kroeze <bkroeze@gmail.com>
2021-05-13 17:41:21 -0700

    Vuethereum wallet component complete

--- src/components/WalletStatus.vue
+++ src/components/WalletStatus.vue
@@ -1,25 +1,45 @@
 <template>
   <vth-status deferred>
     <template slot-scope="web3">
-      <p v-if="web3.loading">{{ loadingMessage }}</p>
+      <div v-if="web3.loading">{{ loadingMessage }}</div>
 
-      <p v-else-if="web3.account">
-        Your address: {{ web3.account }},
-        Network version: {{ web3.networkVersion }}
-      </p>
+      <div v-else-if="web3.account">
+        <div>
+          {{ netName(web3.networkVersion) }}
+        </div>
+        <LimitedText :limit=accountLimit :text="web3.account"></LimitedText>
+      </div>
 
-      <p v-else>
+      <div v-else>
         <q-btn @click="web3.getWeb3" :label="connectMessage" />
-      </p>
+      </div>
     </template>
   </vth-status>
 </template>
 
 <script>
+
+ import { pathOr } from 'ramda'
+ import LimitedText from 'components/LimitedText.vue'
+
+const NETWORKS = {
+   '137': { name: 'Polygon Mainnet', symbol: 'MATIC' },
+   '1': { name: 'Ethereum Mainnet', symbol: 'ETH' },
+   '1666600000': { name: 'Harmony Mainnet/0', symbol: 'ONE' },
+   '0x63564c40': { name: 'Harmony Mainnet/0', symbol: 'ONE' },
+   '56': { name: 'Binance Smart Chain Mainnet', symbol: 'BNB' }
+ }
+
+const netName = (netId) => pathOr(`Unknown Network ${netId}`, [netId, 'name'], NETWORKS)
  
  export default {
    name: 'WalletStatus',
+   components: { LimitedText },
    props: {
+     accountLimit: {
+       type: Number,
+       default: 16
+     },
      connectMessage: {
        type: String,
        default: 'Connect Wallet'
@@ -31,7 +51,10 @@
      deferred: {
        type: Boolean,
        default: true
-     }
+     },
+   },
+   methods: {
+     netName
    }
  }
 </script>
